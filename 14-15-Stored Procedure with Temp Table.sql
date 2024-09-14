005dcdef809c3b032f20ff1ee57cb408d0f7292d
wasim atta <wasim.atta33@hotmail.com>
2024-09-15 00:18:53 +0300

    Editing the Stored Procedure with Temp Table sql , Last One have some Problem

--- 14-15-Stored Procedure with Temp Table.sql
+++ 14-15-Stored Procedure with Temp Table.sql
@@ -10,19 +10,23 @@ BEGIN
     CREATE TABLE #OverdueBorrowers
     (
         BorrowerID INT,
+        BookID INT,
         FirstName NVARCHAR(100),
         LastName NVARCHAR(100)
     );
 
-    INSERT INTO #OverdueBorrowers (BorrowerID, FirstName, LastName)
-    SELECT B.BorrowerID,B.FirstName,B.LastName
+    INSERT INTO #OverdueBorrowers (BorrowerID, FirstName, LastName,BookID)
+    SELECT B.BorrowerID,B.FirstName,B.LastName,L.BookID
     FROM Borrowers B JOIN Loans L ON B.BorrowerID = L.BorrowerID
-    WHERE L.DateReturned IS NULL AND L.DueDate < GETDATE();
+    WHERE L.DateReturned IS NULL or L.DueDate < L.DateReturned;
+
+    select * from #OverdueBorrowers;
 
     SELECT OB.BorrowerID, OB.FirstName, OB.LastName,L.BookID, L.DateBorrowed,L.DueDate,
-           DATEDIFF(DAY, L.DueDate, GETDATE()) AS OverdueDays
-    FROM #OverdueBorrowers OB JOIN  Loans L ON OB.BorrowerID = L.BorrowerID
-    WHERE L.DateReturned IS NULL AND L.DueDate < GETDATE();
+           IIF(L.DateReturned is null, DATEDIFF(DAY, L.DueDate, GETDATE()), DATEDIFF(DAY, L.DueDate, L.DateReturned)) as OverdueDays
+    FROM #OverdueBorrowers OB JOIN  Loans L ON OB.BorrowerID = L.BorrowerID AND OB.BookID = L.BookID
+    WHERE L.DateReturned IS NULL or L.DueDate < L.DateReturned
+    order by OB.BorrowerID;
 
     DROP TABLE #OverdueBorrowers;
 END;
